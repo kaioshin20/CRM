9b7305ad5d538aeaab5c1fc375ab664af9a76906
tanuj <tanujvyas10@gmail.com>
2021-06-12 19:15:17 +0530

    config

---
+++ server/config/secret.js
@@ -0,0 +1,6 @@
+module.exports={
+   url: "mongodb+srv://amit:amit123@cluster0-jmjnh.mongodb.net/quloi1?retryWrites=true&w=majority",
+      //    url:'mongodb://localhost/Quloi_db',
+    secret:"Myjsonsecret"
+       
+};
